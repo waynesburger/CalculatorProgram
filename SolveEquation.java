1f207adc6f633adfc495b20a42a51b8c1f7f5faf
waynesburger <freeholays@hotmail.com>
2017-10-24 20:12:56 -0700

    Add files via upload
    Class that solves the equation in postfix mode

---
+++ SolveEquation.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+import java.util.ArrayList;
+
+public class SolveEquation{
+   
+   public static double returnAnswer(ArrayList<Object> results){
+   
+      Stack<Double> stak = new Stack<Double>();
+      Object element;
+      Double first, second;
+      int conversion1;
+      double conversion2;
+      
+      //each member of the results is processed in order
+      for(int a=0; a<results.size(); a++){
+         //get the next element in results
+         element = results.get(a);
+         
+         //push into stak if it's a number
+         //some conversions needed to be done in order 
+              //for the number to be accepted into stak
+         if(element instanceof Number){
+            conversion1 = (int)element;
+            conversion2 = (double)conversion1;
+            stak.push((Double)insert2);
+         }
+         //if it's an operator
+         //pop the top two number from stak and perform the appropriate opperation
+         else{
+            second = stak.pop();
+            first = stak.pop();
+            
+            if(element.equals("+")){
+               stak.push(first+second);
+            }else if(element.equals("-")){
+               stak.push(first-second);
+            }else if(element.equals("x") || element.equals("*")){
+               stak.push(first*second);
+            }else if(element.equals("/")){
+               stak.push(first/second);
+            }else if(element.equals("%")){
+               stak.push(first%second);
+            }   
+         }
+      }
+      
+      //in the end, stak should contain one number
+      //pop it from the stack and return it
+      double answer = stak.pop();
+      return answer;
+   }
+   
+}
